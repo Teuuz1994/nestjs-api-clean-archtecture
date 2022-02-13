4552943098919100b0eaab8afa0edb0bd95a2163
Matheus da Guia de Campos <matheus.rj.ig@gmail.com>
2022-02-13 16:54:57 -0300

    chore: change port exposed

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,9 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  const port = process.env.PORT || 3333;
+  await app.listen(port, () =>
+    console.info('[INFO] Server is running on http://localhost:3333'),
+  );
 }
 bootstrap();
