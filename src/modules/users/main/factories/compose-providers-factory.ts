0becbf36ee6729da673ede2908816c6f207bd0a7
Matheus Campos <matheus.rj.ig@gmail.com>
2022-02-14 00:26:06 -0300

    refactor: compose providers in factory method

---
+++ src/modules/users/main/factories/compose-providers-factory.ts
@@ -0,0 +1,42 @@
+import {
+  CreateUserUseCase,
+  DeleteUserUseCase,
+  LIstAllUsersUserCase,
+  UpdateUserUseCase,
+} from '../../data/use-cases';
+import { GenerateUuidAdapter } from '../../infra/generator/id-generator';
+import { CypherCompare, HashAdapter } from '../../infra/hash-provider';
+import { TOKEN_INJECTION } from '../../infra/tokens/token-injection';
+
+export const composeProvidersFactory = () => {
+  return [
+    {
+      provide: TOKEN_INJECTION.HASH_PROVIDER,
+      useClass: HashAdapter,
+    },
+    {
+      provide: TOKEN_INJECTION.DB_LIST_ALL_USERS,
+      useClass: LIstAllUsersUserCase,
+    },
+    {
+      provide: TOKEN_INJECTION.DB_CREATE_USER,
+      useClass: CreateUserUseCase,
+    },
+    {
+      provide: TOKEN_INJECTION.DB_UPDATE_USER,
+      useClass: UpdateUserUseCase,
+    },
+    {
+      provide: TOKEN_INJECTION.COMPARE_HASH_PROVIDER,
+      useClass: CypherCompare,
+    },
+    {
+      provide: TOKEN_INJECTION.DB_DELETE_USER,
+      useClass: DeleteUserUseCase,
+    },
+    {
+      provide: TOKEN_INJECTION.ID_GENERATOR,
+      useClass: GenerateUuidAdapter,
+    },
+  ];
+};
