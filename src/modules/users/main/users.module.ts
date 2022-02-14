06c54021176a09720c392ad815629d677198516f
Matheus Campos <matheus.rj.ig@gmail.com>
2022-02-14 00:27:25 -0300

    refactor: invoke factories controllers and providers in user module

--- src/modules/users/main/users.module.ts
+++ src/modules/users/main/users.module.ts
@@ -2,61 +2,14 @@ import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 
 import { UserRepository } from '../infra/typeorm/repository/user-repository';
-import { TOKEN_INJECTION } from '../infra/tokens/token-injection';
-import { HashAdapter, CypherCompare } from '../infra/hash-provider';
-import { GenerateUuidAdapter } from '../infra/generator/id-generator';
-
 import {
-  CreateUserUseCase,
-  LIstAllUsersUserCase,
-  UpdateUserUseCase,
-  DeleteUserUseCase,
-} from '../data/use-cases';
-
-import {
-  ListUsersController,
-  CreateUserController,
-  UpdateUserController,
-  DeleteUserController,
-} from '../presentation/controllers';
+  composeControllersFactory,
+  composeProvidersFactory,
+} from './factories';
 
 @Module({
   imports: [TypeOrmModule.forFeature([UserRepository])],
-  controllers: [
-    ListUsersController,
-    CreateUserController,
-    UpdateUserController,
-    DeleteUserController,
-  ],
-  providers: [
-    {
-      provide: TOKEN_INJECTION.HASH_PROVIDER,
-      useClass: HashAdapter,
-    },
-    {
-      provide: TOKEN_INJECTION.DB_LIST_ALL_USERS,
-      useClass: LIstAllUsersUserCase,
-    },
-    {
-      provide: TOKEN_INJECTION.DB_CREATE_USER,
-      useClass: CreateUserUseCase,
-    },
-    {
-      provide: TOKEN_INJECTION.DB_UPDATE_USER,
-      useClass: UpdateUserUseCase,
-    },
-    {
-      provide: TOKEN_INJECTION.COMPARE_HASH_PROVIDER,
-      useClass: CypherCompare,
-    },
-    {
-      provide: TOKEN_INJECTION.DB_DELETE_USER,
-      useClass: DeleteUserUseCase,
-    },
-    {
-      provide: TOKEN_INJECTION.ID_GENERATOR,
-      useClass: GenerateUuidAdapter,
-    },
-  ],
+  controllers: composeControllersFactory(),
+  providers: composeProvidersFactory(),
 })
 export class UsersModule {}
