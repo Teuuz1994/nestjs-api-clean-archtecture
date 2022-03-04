c6c68f3172bd462c272dc0fa3e80528926f45ef0
Matheus da Guia de Campos <matheus.rj.ig@gmail.com>
2022-03-04 00:19:06 -0300

    add e2e tests

--- test/app.e2e-spec.ts
+++ test/app.e2e-spec.ts
@@ -1,12 +1,15 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { INestApplication } from '@nestjs/common';
 import * as request from 'supertest';
-import { AppModule } from './../src/app.module';
 
+import { AppModule } from './../src/app.module';
+import { userMock } from '../src/modules/users/infra/typeorm/test/mocks/user-mock';
+
+const mock = userMock();
 describe('AppController (e2e)', () => {
   let app: INestApplication;
 
-  beforeEach(async () => {
+  beforeAll(async () => {
     const moduleFixture: TestingModule = await Test.createTestingModule({
       imports: [AppModule],
     }).compile();
@@ -15,10 +18,19 @@ describe('AppController (e2e)', () => {
     await app.init();
   });
 
-  it('/ (GET)', () => {
-    return request(app.getHttpServer())
-      .get('/')
-      .expect(200)
-      .expect('Hello World!');
+  it('Should be able to create user using path /users (POST)', async () => {
+    const response = await request(app.getHttpServer())
+      .post('/users')
+      .send(mock);
+
+    expect(response.status).toBe(201);
+    expect(response.body).toHaveProperty('id');
+  });
+
+  it('Should be able to list all users using path /users (GET)', async () => {
+    const response = await request(app.getHttpServer()).get('/users');
+
+    expect(response.status).toBe(200);
+    expect(response.body).toHaveLength(response.body.length);
   });
 });
